28aeea4bf2f7c2838706e9b7d41254cc84d6fb8c
JiayiPang <stella7023@outlook.com>
2021-11-25 10:08:18 -0800

    router setup

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+// Connect database
+connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
 
 const PORT = process.env.PORT || 5000;
+
+// Define routers
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
