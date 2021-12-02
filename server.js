5878ca4bacced860aba85a066c3552588d4f2ca8
JiayiPang <stella7023@outlook.com>
2021-12-01 21:59:32 -0800

    user register, login, authenticate and get token

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 // Connect database
 connectDB();
 
+// Init Middleware
+app.use(express.json({extended: false}));
+
 app.get('/', (req, res) => res.send('API Running'));
 
 const PORT = process.env.PORT || 5000;
